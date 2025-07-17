847c44f50a4c4175618cb870a703199558215aa4
Jango <101262528+Jangveersoothe@users.noreply.github.com>
2025-07-17 17:10:20 +0530

    Update authController.js

--- authController.js
+++ authController.js
@@ -11,23 +11,44 @@ const generateToken = (userId) => {
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
 
-  pool.query(
-    "INSERT INTO users(email, password) VALUES (?,?)",
-    [email, hashedPassword],
-    (err, result) => {
-      if (err) {
-        return res.status(500).json({ error: "Invalid Username or Password" });
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    pool.query(
+      "INSERT INTO users (email, password) VALUES (?, ?)",
+      [email, hashedPassword],
+      (err, result) => {
+        if (err) {
+          return res.status(500).json({ error: "Email already exists or invalid" });
+        }
+
+        const userId = result.insertId;
+        const token = generateToken(userId);
+
+        const response_script = {
+          success: true,
+          message: "Signup successful",
+          user: {
+            id: userId,
+            email,
+            is_active: true,
+            token
+          }
+        };
+
+        res.status(201).json(response_script);
       }
-      const token = generateToken(result.insertId);
-      res.status(201).json({ token });
-    }
-  );
+    );
+  } catch (err) {
+    res.status(500).json({ error: "Something went wrong during signup" });
+  }
 };
 
+
 const login = (req, res) => {
-  [email, password] = req.body;
+  const {email, password} = req.body;
 
   pool.query (
     "SELECT * FROM users WHERE email = ?", [email],async (err, result) =>{
@@ -40,14 +61,26 @@ const login = (req, res) => {
 
       const user = result[0];
 
-      const isMatch = await bcrypt.compare(password,hashedPassword);
+      const isMatch = await bcrypt.compare(password,user.password);
 
       if(!isMatch){
         return res.status(401).json({error: "Wrong password"});
       }
 
+
       const token = generateToken(user.id);
-      res.json({token});
+      const response_script = {
+          success: true,
+          message: "Login successful",
+          user: {
+            id: user.Id,
+            email,
+            is_active: true,
+            token
+          }
+        };
+
+      res.status(200).json({response_script});
 
     }
   )
